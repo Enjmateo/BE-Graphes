2a7dd971bc8ae737e0c0f1cafa56d0bb87d2c44e
Enjmateo <enjmateo@yahoo.fr>
2021-03-25 01:20:25 +0100

    modifications config pom et supression des @deprecated inutiles

--- be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Path.java
@@ -30,7 +30,6 @@
          * @throws IllegalArgumentException If the list of nodes is not valid, i.e. two
          *         consecutive nodes in the list are not connected in the graph.
          * 
-         * @deprecated Need to be implemented.
          */
         public static Path createFastestPathFromNodes(Graph graph, List<Node> nodes)
                 throws IllegalArgumentException {
@@ -70,7 +69,6 @@
          * @throws IllegalArgumentException If the list of nodes is not valid, i.e. two
          *         consecutive nodes in the list are not connected in the graph.
          * 
-         * @deprecated Need to be implemented.
          */
         public static Path createShortestPathFromNodes(Graph graph, List<Node> nodes)
                 throws IllegalArgumentException {
