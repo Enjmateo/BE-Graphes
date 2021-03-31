8c7cca825b3c6e9b12de37f52205fbd8dec2b3eb
Enjmateo <enjmateo@yahoo.fr>
2021-03-31 18:18:16 +0200

    Création classe Label Algorithme (à continuer)

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,6 +1,33 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.model.Arc;
+import org.insa.graphs.model.Graph;
+import org.insa.graphs.model.Node;
+import org.insa.graphs.model.Path;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
+    class Label{
+        Node sommetCourant;
+        boolean marque;
+        float cout;
+        Node pere;
+
+        public Label(Node sommetCourant, boolean marque, float cout, Node pere) {
+            this.sommetCourant = sommetCourant;
+            this.marque = marque;
+            this.cout = cout;
+            this.pere = pere;
+        }
+
+        public float getCost(){
+            return this.cout;
+        }
+    }
 
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
